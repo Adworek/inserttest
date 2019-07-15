320a62b8b9b14cfe03bd1cbcd6726306817c9f6c
Andrew <50060386+Adworek@users.noreply.github.com>
2019-07-15 17:04:20 -0400

    Added WIP user interface code

---
+++ usermenutest.py
@@ -0,0 +1,4 @@
+from configparser import ConfigParser
+
+parser['inserttest_config']['size'] = input("Enter insert size: ")
+parser['inserttest_config']['quantity'] = input ("Enter quantity to break (enter 0 if unsure): ")
